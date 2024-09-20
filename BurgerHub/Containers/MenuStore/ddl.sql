7fd091cc9f5c649216315efff662667bacd81234
Amisitris <amir.hindawi02@gmail.com>
2024-09-20 10:42:10 +0200

    created table for menu

---
+++ BurgerHub/Containers/MenuStore/ddl.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS MenuTb;
+
+CREATE TABLE `MenuTb`
+(
+
+    `burger_name` VARCHAR (40),
+    `patty` VARCHAR (40),
+    `toppings` VARCHAR (120),
+    `sauce` VARCHAR (40)
+    `sidedish` VARCHAR (40)
+    `drink` VARCHAR (40)
+
+)
