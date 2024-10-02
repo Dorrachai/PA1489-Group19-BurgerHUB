0f7af9cbf6409a25218a79b5738b0d931396a773
Amisitris <Amir.hindawi02@gmail.com>
2024-10-02 12:25:29 +0200

    fixed small syntax in routes

--- BurgerHub/Containers/routes/routes.js
+++ BurgerHub/Containers/routes/routes.js
@@ -15,9 +15,14 @@ router.get("/order", (req, res) => {
     res.render("./../Containers/views/pages/order.ejs");
 });
 
-router.get("/place-order", (req, res) => {
+router.get("/place-order", async (req, res) => {
     const burger = req.query.burger;
-    res.render("./../Containers/views/pages/place-order.ejs", { burger });
+    console.log("1");
+    console.log(burger);
+    const burgerInfo = await src.editOrder(burger);
+    console.log("2");
+    console.log(burgerInfo);
+    res.render("./../Containers/views/pages/place-order.ejs", { burger, burgerInfo });
 });
 
 
