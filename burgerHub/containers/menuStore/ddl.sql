759e32fcb8fc6966178402081209b770208abcd1
Amisitris <Amir.hindawi02@gmail.com>
2024-10-03 11:55:00 +0200

    DONT MERGE, but it work. I fixed so that place.order works but only localy and docker not so much. DONT MERGEgit add .git add .

---
+++ burgerHub/containers/menuStore/ddl.sql
@@ -0,0 +1,39 @@
+DROP TABLE IF EXISTS MenuTb;
+DROP TABLE IF EXISTS SidesTb;
+DROP TABLE IF EXISTS DrinksTb;
+DROP TABLE IF EXISTS OrdersTb;
+
+CREATE TABLE `MenuTb`
+(
+
+    `burger_name` VARCHAR (40),
+    `patty` VARCHAR (40),
+    `toppings` VARCHAR (120),
+    `sauce` VARCHAR (40),
+    `sidedish` VARCHAR (40),
+    `drink` VARCHAR (40)
+
+);
+
+CREATE TABLE `SidesTb`
+(
+
+    `side_name` VARCHAR (120)
+
+);
+
+CREATE TABLE `DrinksTb`
+(
+
+    `drink_name` VARCHAR (120)
+
+);
+
+CREATE TABLE `OrdersTb`
+(
+    `order_id` int NOT NULL AUTO_INCREMENT UNIQUE,
+    `burger` VARCHAR (40),
+    `sides` VARCHAR (40),
+    `drink` VARCHAR (40)
+
+);
---
+++ burgerHub/containers/routes/routes.js
@@ -0,0 +1,34 @@
+"use strict";
+
+const express = require("express");
+const router = express.Router();
+const src = require("/home/amir/1489/PA1489-Group19-BurgerHUB/burgerHub/containers/burgerOrderer/burgerorderer.js");
+
+
+router.get("/", (req, res) => {
+
+    res.render("./../containers/views/pages/index.ejs");
+});
+
+router.get("/order", (req, res) => {
+
+    res.render("./../containers/views/pages/order.ejs");
+});
+
+router.get("/place-order", async (req, res) => {
+    try {
+        const burger = req.query.burger;
+        const sides = await src.editOrder();  // Fetch the sides array from the database
+        console.log(sides);
+
+        // Ensure sides is passed as an array
+        res.render("./../containers/views/pages/place-order.ejs", { burger, sides });
+    } catch (error) {
+        console.error("Error fetching data for place-order:", error);
+        res.status(500).send("An error occurred");
+    }
+});
+
+
+
+module.exports = router;
